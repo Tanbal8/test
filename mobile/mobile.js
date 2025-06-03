b5e04571f9c75df00d0c6434a66e15b8bed4ac4f
Tanbal <tanbal.derakhty@gmail.com>
2025-06-04 00:15:51 +0330

    mobile updated

--- mobile/mobile.js
+++ mobile/mobile.js
@@ -1,1 +1,7 @@
 var element = document.getElementById("element");
+var status = document.getElementById("status");
+console.log(element);
+window.ontouchmove = function(e) {
+    console.log(e);
+    status.innerHTML = e;
+}
