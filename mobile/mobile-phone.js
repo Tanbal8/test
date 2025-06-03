870ef415d5a7c5143f5df19b12ec243cc868861e
Tanbal <tanbal.derakhty@gmail.com>
2025-06-04 01:32:52 +0330

    Update mobile-phone.js

--- mobile/mobile-phone.js
+++ mobile/mobile-phone.js
@@ -2,7 +2,6 @@ var element = document.getElementById("element");
 var status_div = document.getElementById("status");
 status_div.innerHTML = "Mobile";
 element.ontouchmove = function(e) {
-    status_div.innerHTML += `<br>(${e.touches[0].clientX}, ${e.touches[0].clientY})`;
     element.style.top = e.touches[0].clientY.toFixed(2) + "px";
     element.style.left = e.touches[0].clientX.toFixed(2) + "px";
 }
