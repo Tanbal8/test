62d58e42bf5f5949725d35aaa2e0959e0e0476fe
Tanbal <tanbal.derakhty@gmail.com>
2025-06-04 00:32:10 +0330

    Update mobile.js

--- mobile/mobile.js
+++ mobile/mobile.js
@@ -3,7 +3,7 @@ var status_div = document.getElementById("status");
 console.log(element);
 window.ontouchmove = function(e) {
     console.log(e);
-    status_div.innerHTML = e;
+    status_div.innerHTML += `<br>(${e.clientY}, ${e.clientX})`;
 }
 element.onclick = function(e) {
     status_div.innerHTML += `<br>(${e.clientY}, ${e.clientX})`;
