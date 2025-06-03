186baf645a67ec5aefa00d8c02001d016cbe65c8
Tanbal <tanbal.derakhty@gmail.com>
2025-06-04 01:15:11 +0330

    mobile

--- mobile/mobile-phone.js
+++ mobile/mobile-phone.js
@@ -2,9 +2,10 @@ var element = document.getElementById("element");
 var status_div = document.getElementById("status");
 status_div.innerHTML = "Mobile";
 console.log(element);
-window.ontouchmove = function(e) {
-    console.log(e);
-    status_div.innerHTML += `<br>(${e.clientY}, ${e.clientX})`;
+element.ontouchmove = function(e) {
+    // status_div.innerHTML += `<br>(${e.touches[0].clientX}, ${e.touches[0].clientY})`;
+    element.style.top = e.touches[0].clientY.toFixed(2) + "px";
+    element.style.left = e.touches[0].clientX.toFixed(2) + "px";
 }
 element.onclick = function(e) {
     status_div.innerHTML += `<br>(${e.clientY}, ${e.clientX})`;
