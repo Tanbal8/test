3389a7495eaea5a7dbbeb4f01526768165348d65
Tanbal <tanbal.derakhty@gmail.com>
2025-06-04 00:47:06 +0330

    Update mobile-pc.js

--- mobile/mobile-pc.js
+++ mobile/mobile-pc.js
@@ -3,7 +3,7 @@ var element = document.getElementById("element");
 var status_div = document.getElementById("status");
 console.log(element);
 element.onmousemove = function(e) {
-    console.log(e);
+    console.log(e.clientY, e.clientX);
 }
 element.onclick = function(e) {
     console.log(e.clientY, e.clientX);
